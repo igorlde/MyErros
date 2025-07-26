dde1600c221f43147e70aa7953c777c21eaa6cdd
igorlde <igorligeiro46@gmail.com>
2025-07-26 16:11:39 +0000

    feat: add routes for utility codes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProblemController;
+use App\Http\Controllers\SendSolutionController;
 use Illuminate\Support\Facades\Route;
 
 //route for page home.
@@ -9,15 +10,12 @@ Route::get('/',
 
 //route group for all my routes of problem
 Route::group(['prefix', 'problem'], function(){
-//route for page create code.
 Route::get('/problem',
 [ProblemController::class, 'create'])->name('problem');
 
-//route for store the datas of form.
 Route::post('/store/problem',
 [ProblemController::class, 'store'])->name('store.problem');
 
-//is route to show datas from a problem who you seek
 Route::get('/show/{id}',
 [ProblemController::class, 'show'])->name('show.problem');
 
@@ -28,3 +26,22 @@ Route::put('/update/{id}',
 [ProblemController::class, 'update'])->name('update.problem');
 
 })->name('app.problems');
+
+//group for utility codes
+Route::group(['prefix', 'utility'], function(){
+    Route::get('/solution', 
+    [SendSolutionController::class, 'create'])->name('create.solution');
+
+    Route::post('/store/solution', 
+    [SendSolutionController::class, 'store'])->name('store.solution');
+
+    Route::get('/show/{id}', 
+    [SendSolutionController::class, 'show'])->name('show.solution');
+
+    Route::get('/edit/{id}',
+    [SendSolutionController::class, 'edit'])->name('edit.solution');
+
+    Route::put('/update/{id}',
+    [SendSolutionController::class, 'update'])->name('update.solution');
+
+})->name('app.utility');
